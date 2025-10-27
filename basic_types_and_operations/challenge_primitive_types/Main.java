e1c30be366945afd218b917fef5f5732e54e858e
romandmytrynskyi-jpg <>
2025-10-27 10:26:16 +0000

    Challenge: Primitive Types

---
+++ basic_types_and_operations/challenge_primitive_types/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        ___ myNum = 14;
+        ___ myFractional = 14.928;
+        ___ myBiggerNum = -710;
+        ___ myLetter = 'B';
+        ___ myBool = true;
+        ___ myWho = 1.74f;
+        System.out.println("It's working, well done!");
+    }
+}
